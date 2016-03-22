93cb243fd2d0033cb5e3f2e24bea6bbee7065703
Alexandru Pascal <apascal07@gmail.com>
2016-03-22 12:44:43 -0400

    Installed linter and fixed style issues (disabled 4-space indent rule).

--- models.py
+++ models.py
@@ -1,6 +1,15 @@
 import enum
 from google.appengine.ext import ndb
 
+
+class Status(enum.Enum):
+  """Enumerates the states that a TrainingSet can be in."""
+  idle = 0
+  running = 1
+  interrupted = 2
+  finished = 3
+
+
 class Review(ndb.Model):
   """A Review object contains a single review for a single product."""
   # data retrieved from scraping the Amazon pages
@@ -16,6 +25,7 @@ class Review(ndb.Model):
   text_quality = ndb.FloatProperty(indexed=False)
   weight = ndb.FloatProperty(indexed=False)
 
+
 class Reviewer(ndb.Model):
   """A Reviewer object contains all of the data associated with a reviewer."""
   # data retrieved from scraping the Amazon pages
@@ -28,6 +38,7 @@ class Reviewer(ndb.Model):
   average_rating = ndb.FloatProperty(indexed=False)
   standard_deviation = ndb.FloatProperty(indexed=False)
   weight = ndb.FloatProperty(indexed=False)
+
 
 class Product(ndb.Model):
   """A Product object contains meta data and references to its reviews."""
@@ -43,6 +54,7 @@ class Product(ndb.Model):
   average_rating = ndb.FloatProperty(indexed=False)
   weighted_rating = ndb.FloatProperty(indexed=False)
 
+
 class TrainingSet(ndb.Model):
   """A TrainingSet object contains meta data about a training attempt."""
   start_timestamp = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
@@ -51,12 +63,5 @@ class TrainingSet(ndb.Model):
   review_count_limit = ndb.IntegerProperty(indexed=False)
   weights = ndb.PickleProperty(indexed=False)
   product_sample = ndb.KeyProperty(indexed=True, key='Product',
-      repeated=True)
+                                   repeated=True)
   status = ndb.PickleProperty(indexed=False, default=Status.idle)
-
-class Status(enum.Enum):
-  """Enumerates the states that a TrainingSet can be in."""
-  idle = 0
-  running = 1
-  interrupted = 2
-  finished = 3
