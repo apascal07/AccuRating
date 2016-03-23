1f0fb7bbc5da2c7376e4c4ce0c4ceddc13a816b2
Alexandru Pascal <apascal07@gmail.com>
2016-03-23 18:33:16 -0400

    Fixing Adam's shit.

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import ndb
 
+
 class Product(ndb.Model):
   """A Product object contains meta data and references to its reviews."""
   # id = ASIN of the product (e.g. B00I5FWWS0)
@@ -19,6 +20,7 @@ class Product(ndb.Model):
   average_rating = ndb.FloatProperty(indexed=False)
   weighted_rating = ndb.FloatProperty(indexed=False)
   standard_deviation = ndb.FloatProperty(indexed=False)
+
 
 class Review(ndb.Model):
   """A Review object contains a single review for a single product."""
@@ -52,27 +54,6 @@ class Reviewer(ndb.Model):
   weight = ndb.FloatProperty(indexed=False)
 
 
-class Product(ndb.Model):
-  """A Product object contains meta data and references to its reviews."""
-  # id = ASIN of the product (e.g. B00I5FWWS0)
-  # data retrieved from scraping the Amazon pages
-  title = ndb.StringProperty(indexed=True, required=True)
-  product_url = ndb.StringProperty(indexed=True, required=True)
-  reviews_url = ndb.StringProperty(indexed=False, required=True)
-  description = ndb.TextProperty(indexed=False)
-  reviews = ndb.KeyProperty(indexed=False, kind='Review', repeated=True)
-  release_date = ndb.DateTimeProperty(indexed=False, required=True)
-  retrieval_date = ndb.DateTimeProperty(indexed=False, required=True,
-                                        auto_now_add=True)
-  category = ndb.StringProperty(indexed=False, required=True)
-  rating_distribution = ndb.PickleProperty(indexed=False, required=True)
-  amazon_rating = ndb.FloatProperty(indexed=False, required=True)
-  # data computed by the system
-  average_rating = ndb.FloatProperty(indexed=False)
-  weighted_rating = ndb.FloatProperty(indexed=False)
-  standard_deviation = ndb.FloatProperty(indexed=False)
-
-
 class TrainingSet(ndb.Model):
   """A TrainingSet object contains meta data about a training attempt."""
   start_timestamp = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
@@ -80,5 +61,4 @@ class TrainingSet(ndb.Model):
   criteria = ndb.PickleProperty(indexed=False)
   review_count_limit = ndb.IntegerProperty(indexed=False)
   weights = ndb.PickleProperty(indexed=False)
-  product_sample = ndb.KeyProperty(indexed=True, kind='Product',
-                                   repeated=True)
+  product_sample = ndb.KeyProperty(indexed=True, kind='Product', repeated=True)
