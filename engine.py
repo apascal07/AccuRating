db7cec9169d5b75ffa6639b4a6793e1c4c1ca802
Alexandru Pascal <apascal07@gmail.com>
2016-03-23 18:38:51 -0400

    Updated create_product() with single review page fetch and update_url().

--- engine.py
+++ engine.py
@@ -1,8 +1,9 @@
 import fetcher
 import logging
-import models
+import parser
 import scraper
-from google.appengine.ext import ndb
+import time
+import urllib
 
 
 def create_product(asin):
@@ -18,49 +19,74 @@ def create_product(asin):
     logging.info('Failed to parse product #{} from Amazon.'.format(asin))
     return None
   # fetch the base review page and find the number of total review pages
-  base_url = product.reviews_url
-  base_page = [fetcher.get_pages([base_url])]
+  url = product.reviews_url
+  base_page = fetcher.fetch_pages([url])
   page_count = scraper.get_page_count(base_page) or 1
-  # compile a list of all of the review page URLs and fetch simultaneously
-  page_urls = [get_url(base_url, page) for page in range(page_count)]
-  review_pages = fetcher.get_pages(page_urls)
-  review_pages.append(base_page)
-  # create Review objects for each review and compile list of reviewer URLs
+  # compile a list of all of the review list URLs and fetch simultaneously
+  page_urls = [update_url(url, {'page': page}) for page in range(page_count)]
+  review_list_pages = fetcher.fetch_pages(page_urls)
+  review_list_pages.append(base_page)
+  # compile a list of all of the review URLs and fetch simultaneously
+  review_urls = []
+  for review_list_page in review_list_pages:
+    review_urls.extend(scraper.get_review_urls(review_list_page))
+  review_pages = fetcher.fetch_pages(review_urls)
+  # create Review and Reviewer objects for each review by fetching profiles
   for review_page in review_pages:
-    reviews = scraper.get_reviews(review_page)
+    review = scraper.get_review(review_page)
     # create Reviewer objects for each reviewer by fetching their profiles
-    for review in reviews:
-      reviewer_page = fetcher.get_profile(review.reviewer_url)
-      reviewer = scraper.get_profile(reviewer_page)
-      # calculate the Reviewer's average rating and standard deviation
-      reviewer.average_rating = get_average(reviewer.rating_distribution)
-      reviewer.standard_deviation = get_deviation(reviewer.rating_distribution)
-      reviewer.put()
-      # associate the Reviewer with the Review and write to datastore
-      review.reviewer = reviewer
-      review.put()
-    # append the new Review objects to the Product object
-    product.reviews.extend(reviews)
+    reviewer_page = fetcher.fetch_profile(review.reviewer_url)
+    reviewer = scraper.get_profile(reviewer_page)
+    # calculate the Reviewer's average rating and standard deviation
+    reviewer.average_rating = get_average(reviewer.rating_distribution)
+    # reviewer.standard_deviation = get_deviation(reviewer.rating_distribution)
+    reviewer.put()
+    # assess the quality of the review text and set the Reviewer object
+    review.text_quality = parser.get_value(review.text)
+    review.reviewer = reviewer.key
+    review.put()
+    # append the new Review object to the Product object
+    product.reviews.append(review.key)
   product.put()
   return product
 
 
+def timer(fn):
+  """Clocks the time it takes a function to execute."""
+  def wrapper(*args, **kwargs):
+    start = time.time()
+    result = fn(*args, **kwargs)
+    end = time.time()
+    delta = end - start
+    logging.info('Executed {} in {} sec(s).'.format(fn.__name__, delta))
+    return result
+  return wrapper
 
-def get_url(base_url, page):
-  pass
 
-
-def get_deviation(a):
-  pass
+def update_url(url, new_params):
+  """Updates the given URL to include (add/replace) the given parameters."""
+  if not new_params:
+    return url
+  # extract any existing parameters in the URL
+  url = urllib.unquote(url)
+  parsed_url = urllib.urlparse(url)
+  params = dict(urllib.parse_qsl(parsed_url.query))
+  # update the parameter dictionary with the new parameters
+  params.update(new_params)
+  # encode the parameter query string and recreate the full URL again
+  query = urllib.urlencode(params)
+  base_url = url.split('?')[0]
+  new_url = '{}?{}'.format(base_url, query)
+  return new_url
 
 
 def get_average(rating_distribution):
   """Computes the average rating based on a distribution ranging from 1-5."""
   if not rating_distribution:
-    logging.warning('Rating distribution is empty.')
-    return None
+    logging.info('Rating distribution is empty.')
+    return 0
   if len(rating_distribution) < 5:
-    logging.warning('Rating distribution has fewer than 5 star buckets.')
+    logging.info('Rating distribution has fewer than 5 star buckets.')
   total = sum([rating * count for rating, count in rating_distribution])
   count = sum(rating_distribution.itervalues())
   return float(total) / count
