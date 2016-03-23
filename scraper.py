f49481351141fdf462e3fcef4773ed686fe6a1ba
Alexandru Pascal <apascal07@gmail.com>
2016-03-23 13:07:49 -0400

    Wrapped module functions in a timer decorator.

--- scraper.py
+++ scraper.py
@@ -1,17 +1,23 @@
 """Scraper extracts data from raw HTML and creates corresponding objects."""
 
+import engine
 
+
+@engine.timer
 def get_page_count(html):
   pass
 
 
+@engine.timer
 def get_reviews(html):
   pass
 
 
+@engine.timer
 def get_profile(html):
   pass
 
 
+@engine.timer
 def get_product(html):
   pass
