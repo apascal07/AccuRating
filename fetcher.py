8e6fce55c730f8a22a5fc998ebb6f7f001ff8119
mvbp <mvater.mvbp@live.com>
2016-04-01 12:12:20 -0400

    Update formatting in fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -55,8 +55,7 @@ class PageFetcher:
     results = []
     # Set the headers to appear as if we are using a real browser.
     # AppEngine plays with these results to state the request is coming from AppEngine
-    hdr = {
-           'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) '
+    hdr = {'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) '
                           'Chrome/23.0.1271.64 Safari/537.11'),
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
