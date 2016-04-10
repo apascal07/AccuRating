3f792a3c5b3ed977862cf945ee9cf5102d6cc0d5
admann99 <amm349@pitt.edu>
2016-04-10 19:52:55 -0400

    Tested Verifier, fixed bugs, and added url handler

--- accurating/views.py
+++ accurating/views.py
@@ -10,6 +10,8 @@ def dashboard(request, **kwargs):
 import models
 import pprint
 import trainer
+import verifier
+import json
 from django import http, shortcuts
 
 
@@ -25,6 +27,7 @@ def search_handler(request, **kwargs):
   #   pprint.pprint(review.__dict__)
   return response
 
+
 def train_handler(request, **kwargs):
   trainer_ = trainer.Trainer()
   ps = [p.asin for p in models.Product.objects.all()]
@@ -34,3 +37,8 @@ def train_handler(request, **kwargs):
     pro = models.Product.get_product(p)
     print 'amaz: {}, avg: {}, smart: {}'.format(pro.amazon_rating, pro.average_rating, pro.weighted_rating)
   return http.HttpResponse()
+
+
+def verification_handler(request):
+  asins = [p.asin for p in models.Product.objects.all()]
+  return http.HttpResponse(str(verifier.verify(asins)))
