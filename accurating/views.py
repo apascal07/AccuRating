c20fe37046857c068b48f788e5e4446942809ee7
Alexandru Pascal <apascal07@gmail.com>
2016-04-12 20:03:30 -0400

    Removed old views.

--- accurating/views.py
+++ accurating/views.py
@@ -37,32 +37,3 @@ def training_view(request, **kwargs):
     pro = models.Product.get_product(p)
     print 'asin: {}, amaz: {}, avg: {}, smart: {}, error: {}'.format(pro.asin, pro.amazon_rating, pro.average_rating, pro.weighted_rating, pro.amazon_rating - pro.weighted_rating)
   return shortcuts.render(request, 'base.html', context)
-
-
-def search_handler(request, **kwargs):
-  pprint.pprint([p.asin for p in models.Product.objects.all()])
-  asin = kwargs['asin']
-  product = models.Product.get_or_create_product(asin)
-  response = http.HttpResponse()
-  response.status_code = 200
-  response.content_type = 'application/json'
-  pprint.pprint(product.__dict__)
-  # for review in product.reviews.all():
-  #   pprint.pprint(review.__dict__)
-  return response
-
-
-def train_handler(request, **kwargs):
-  trainer_ = trainer.Trainer()
-  ps = [p.asin for p in models.Product.objects.all()]
-  tset = trainer_.train(ps)
-  pprint.pprint(tset.__dict__)
-  for p in ps:
-    pro = models.Product.get_product(p)
-    print 'asin: {}, amaz: {}, avg: {}, smart: {}, error: {}'.format(pro.asin, pro.amazon_rating, pro.average_rating, pro.weighted_rating, pro.amazon_rating - pro.weighted_rating)
-  return http.HttpResponse()
-
-
-def verification_handler(request):
-  asins = [p.asin for p in models.Product.objects.all()]
-  return http.HttpResponse(str(verifier.verify(asins)))
